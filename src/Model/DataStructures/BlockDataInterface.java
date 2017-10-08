49d9d738141a658485b4e92b540c6c582ae43e25
alejoaquili <aaquili@itba.edu.ar>
2017-10-08 18:22:49 -0300

    getnode

--- src/Model/DataStructures/BlockDataInterface.java
+++
@@ -1,8 +0,0 @@
-package Model.DataStructures;
-
-/**
- * This interface represents the requirement to conform the data of a {@code Block}.
- */
-public interface BlockDataInterface {
-     boolean contains(Object o);
-}
