ec63cc96ceb27feeb9539c55091c9230d423ad62
Frannx1 <fsanguineti@itba.edu.ar>
2017-10-09 21:30:04 -0300

    Se puede construir avltree a partir de sus operaciones

---
+++ src/Model/DataStructures/AVL/InvalidAVLOperationDataException.java
@@ -0,0 +1,12 @@
+package Model.DataStructures.AVL;
+
+public class InvalidAVLOperationDataException extends Exception {
+
+    public InvalidAVLOperationDataException(String s) {
+        super(s);
+    }
+
+    public InvalidAVLOperationDataException() {
+        super();
+    }
+}
